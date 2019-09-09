a1b3a4ba421b97e2e1fd5f501558bb6af63d4adf
Zabarny Alexander <extenebris.x@gmail.com>
2019-09-09 18:07:44 +0300

    Added react-bootstrap, styled-component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import {Button, InputGroup, FormControl} from 'react-bootstrap';
 import Products from './products/products'
 
 class App extends React.Component {
@@ -13,28 +14,21 @@ class App extends React.Component {
     this.searchBtn = this.searchBtn.bind(this);
     this.searchInputChange = this.searchInputChange.bind(this);
   }
-  componentWillMount(){
-    const request = new XMLHttpRequest();
-    request.open(
-      'GET',
-      'https://demo8421975.mockable.io/products',
-      true);
-    request.send();
-    request.onreadystatechange = () => {
-      if (request.readyState === 4 && request.status === 200) {
-        const response = JSON.parse(request.response).products;
+  componentWillMount() {
+    fetch('https://demo8421975.mockable.io/products')
+      .then(data => data.json()).then(data => {
+        let products = data.products;
         this.setState({
-          products: response
+          products
         })
-      }
-    }
+      })
   }
-  searchInputChange(e){
+  searchInputChange(e) {
     this.setState({
       searchInput: e.target.value.toLowerCase()
     })
   }
-  searchBtn(){
+  searchBtn() {
     const filteredProducts = this.state.products.filter(item => item.name.toLowerCase().search(this.state.searchInput) !== -1);
     this.setState({
       filteredProducts
@@ -42,14 +36,21 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div className="App">
-        <input type="text" onChange={this.searchInputChange}/>
-        <button onClick={this.searchBtn}>search</button>
-          {this.state.filteredProducts? 
-          <Products products={this.state.filteredProducts}/>
+      <div className="App col-lg-12">
+        <InputGroup className="mb-3">
+          <FormControl
+            onChange={this.searchInputChange} 
+          />
+          <InputGroup.Append>
+            <Button variant="outline-secondary" onClick={this.searchBtn}>Search</Button>
+          </InputGroup.Append> 
+        </InputGroup>
+
+        {this.state.filteredProducts ?
+          <Products products={this.state.filteredProducts} />
           :
-          <Products products={this.state.products}/>
-          }
+          <Products products={this.state.products} />
+        }
       </div>
     );
   }
--- src/products/products.js
+++ src/products/products.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import styled from 'styled-components'
 
+const Span = styled.span`
+    font-size: 3vmin;   
+`
+const Li = styled.ul`
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 1px solid black;
+`
 const Products = (props) => {
-    return <ul>
+    return <ul className='col-lg-12'>
         {props.products
-    .map(product => <li>
-        <img src={product.img} alt="product" />
-        <span>{product.price} GBP</span>
-        <span>{product.name}</span>
-    </li>)}
+    .map(product => <Li>
+        <img src={product.img} alt="product" className='col-lg-3'/>
+        <Span className='col-lg-3'>{product.price} GBP</Span>
+        <Span className='col-lg-3'>{product.name}</Span>
+    </Li>)}
     </ul>
 }
 export default Products;
